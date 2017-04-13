44698e1404f2e6bad8b0fb199ec1bec61fd0f5fc
pjshi23 <pjshi23@gmail.com>
2017-04-13 10:17:57 +0800

    Update install-cassandra.sh

--- virtual-machines/n-tier-linux/extensions/linux/install-cassandra.sh
+++ virtual-machines/n-tier-linux/extensions/linux/install-cassandra.sh
@@ -13,19 +13,3 @@ echo opscenter_node_name $opscenter_node_name
 echo data_center_name $data_center_name
 echo location $location
 
-seed_node_dns_name="$seed_node_name.$location.cloudapp.chinacloudapi.cn"
-opscenter_dns_name="$opscenter_node_name.$location.cloudapp.chinacloudapi.cn"
-
-echo "Calling dse.sh with the settings:"
-echo cloud_type $cloud_type
-echo seed_node_dns_name $seed_node_dns_name
-echo data_center_name $data_center_name
-echo opscenter_dns_name $opscenter_dns_name
-
-apt-get -y install unzip
-
-wget https://github.com/DSPN/install-datastax-ubuntu/archive/master.zip
-unzip master.zip
-cd install-datastax-ubuntu-master/bin
-
-./dse.sh $cloud_type $seed_node_dns_name $data_center_name $opscenter_dns_name
