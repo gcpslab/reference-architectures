a3de182f1b784e9ea3bf5fe8a1ddd294820463a6
pjshi23 <pjshi23@gmail.com>
2017-04-13 10:19:08 +0800

    Update install-opscenter.sh

--- virtual-machines/n-tier-linux/extensions/linux/install-opscenter.sh
+++ virtual-machines/n-tier-linux/extensions/linux/install-opscenter.sh
@@ -15,10 +15,3 @@ echo "Calling opscenter.sh with the settings:"
 echo cloud_type $cloud_type
 echo seed_node_dns_name $seed_node_dns_name
 
-apt-get -y install unzip
-
-wget https://github.com/DSPN/install-datastax-ubuntu/archive/master.zip
-unzip master.zip
-cd install-datastax-ubuntu-master/bin
-
-./opscenter.sh $cloud_type $seed_node_dns_name
